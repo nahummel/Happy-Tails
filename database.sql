7bb33b0e18494babc541e08993017aae60aef75b
Natalie Hummel <natoms225@gmail.com>
2020-05-26 11:21:39 -0500

    created database tables

--- database.sql
+++ database.sql
@@ -1,10 +1,71 @@
+--BASE--
 
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
-CREATE TABLE "user" (
+CREATE TABLE "users" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "f_name" VARCHAR (20),
+    "l_name" VARCHAR (20),
+    "street" VARCHAR (255),
+    "city" VARCHAR (50),
+    "state" VARCHAR (25),
+    "zipcode" INT,
+    "phone" INT,
+    "email" VARCHAR (255),
+    "quest_id" INT 
+);
+
+CREATE TABLE "rescues" (
+    "id" SERIAL PRIMARY KEY,
+    "username" VARCHAR (80) UNIQUE NOT NULL,
+    "password" VARCHAR (1000) NOT NULL,
+    "name" VARCHAR (50),
+    "street" VARCHAR (255),
+    "city" VARCHAR (50),
+    "state" VARCHAR (25),
+    "zipcode" INT,
+    "phone" INT,
+    "email" VARCHAR (255),
+);
+
+CREATE TABLE "dogs" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (50),
+    "breed" VARCHAR (50),
+    "sex" VARCHAR (6),
+    "age" INT,
+    "rescue_id" INT,
+    "quest_id" INT
+);
+
+CREATE TABLE "questionnaires" (
+    "id" SERIAL PRIMARY KEY,
+    "size" VARCHAR (20),
+    "age_range" VARCHAR (10),
+    "sex" VARCHAR (6),
+    "rent_breed" BOOLEAN,
+    "other_dogs" BOOLEAN,
+    "cats" BOOLEAN,
+    "kids" BOOLEAN,
+    "grooming" BOOLEAN,
+    "active" BOOLEAN,
+    "training" BOOLEAN,
+    "working_breed" BOOLEAN,
+    "health" BOOLEAN,
+    "dog_id" INT,
+    "user_id" INT
+);
+
+CREATE TABLE "matches" (
+    "id" SERIAL PRIMARY KEY,
+    "dog_id" INT,
+    "user_id" INT
+);
+
+--STRETCH--
+
+CREATE TABLE "favorites" (
+    "id" SERIAL PRIMARY KEY,
+    "dog_id" INT,
+    "user_id" INT
 );
