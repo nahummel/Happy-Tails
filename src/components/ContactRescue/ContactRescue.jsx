e2c6702c1f096a823df44a20af141fecaab4e2fe
Natalie Hummel <natoms225@gmail.com>
2020-06-06 16:13:25 -0500

    Styled Contact Rescue Page

--- src/components/ContactRescue/ContactRescue.jsx
+++ src/components/ContactRescue/ContactRescue.jsx
@@ -1,54 +1,116 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import { withStyles } from "@material-ui/core/styles";
+import SendIcon from "@material-ui/icons/Send";
 
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-class ContactReacue extends Component {
-    state = {
-        reciever: this.props.rescue.email,
-        sender: this.props.user.email,
-        emailText: ''
-    }
+const styles = {
+  contact: {
+    margin: 75,
+    width: 500,
+    padding: "30px 50px 50px",
+    textAlign: "center",
+  },
+  title: {
+    textAlign: "center",
+    marginBottom: 30,
+  },
+  input: {
+    display: "block",
+    marginBottom: 15,
+  },
+  btnContainer: {
+    textAlign: "right",
+  },
+  button: {
+    marginRight: 15,
+  },
+};
 
-    handleBack = () => {
-        this.props.history.push('/about-me')
-    }
+class ContactRescue extends Component {
+  state = {
+    reciever: this.props.rescue.email,
+    sender: this.props.user.email,
+    emailText: `Hi, my name is ${this.props.user.name}, and I am interested in learning more about ${this.props.dog.name}.`,
+  };
 
-    handleChange = (event, property) => {
-        switch (property) {
-            case 'emailText':
-                return this.setState({
-                    [property]: event.target.value
-                })
-            default:
-                return this.state;
-        }
-    }
+  handleBack = () => {
+    this.props.history.goBack();
+  };
 
-    handleClick = () => {
-        this.props.dispatch({type:'SEND_MAIL', payload: this.state})
-        this.props.history.push('/about-me')
+  handleChange = (event, property) => {
+    switch (property) {
+      case "emailText":
+        return this.setState({
+          [property]: event.target.value,
+        });
+      default:
+        return this.state;
     }
+  };
 
-    render() {
-        return (
-            <>
-                <h2>Contact {this.props.rescue.name}</h2>
-                <input type='text' placeholder='I would like to inquire about...'  className='email' onChange={(event) => this.handleChange(event, 'emailText')}></input>
-                <button onClick={this.handleClick}>Send</button>
-                <button onClick={this.handleBack}>Back</button>
-            </>
-        )
-    }
+  handleClick = () => {
+    this.props.dispatch({ type: "SEND_MAIL", payload: this.state });
+    this.props.history.goBack();
+  };
 
+  render() {
+    const { classes } = this.props;
+    return (
+      <Container maxWidth="md">
+        <Grid container direction="row" justify="center">
+          <Paper variant="outlined" className={classes.contact}>
+            <Typography variant="h4" className={classes.title}>
+              Contact {this.props.rescue.name}
+            </Typography>
+            <TextField
+              value={this.state.emailText}
+              variant="outlined"
+              className={classes.input}
+              fullWidth
+              multiline
+              rows={7}
+              onChange={(event) => this.handleChange(event, "emailText")}
+            />
+            <div className={classes.btnContainer}>
+              <Button
+                color="primary"
+                size="medium"
+                className={classes.button}
+                onClick={this.handleBack}
+              >
+                Cancel
+              </Button>
+              <Button
+                onClick={this.handleClick}
+                variant="contained"
+                color="primary"
+                size="medium"
+                endIcon={<SendIcon />}
+              >
+                Send
+              </Button>
+            </div>
+          </Paper>
+        </Grid>
+      </Container>
+    );
+  }
 }
 
-const mapStateToProps = state => ({
-    user: state.user,
-    rescue: state.rescue,
-    dispatch: state.dispatch
+const mapStateToProps = (state) => ({
+  user: state.user,
+  rescue: state.rescue,
+  dog: state.viewDog,
+  dispatch: state.dispatch,
 });
 
-// this allows us to use <App /> in index.js
-export default connect(mapStateToProps)(ContactReacue);
+export default connect(mapStateToProps)(withStyles(styles)(ContactRescue));
