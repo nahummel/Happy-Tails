bbb05c8cb8176bb8440f271b8dd798f5be910224
Natalie Hummel <natoms225@gmail.com>
2020-06-07 14:07:02 -0500

    Add image to info page

--- src/components/InfoPage/InfoPage.js
+++ src/components/InfoPage/InfoPage.js
@@ -12,30 +12,28 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-
 const InfoPage = () => {
   const classes = useStyles();
-  return(
-  <Container  maxWidth="md">
-    <Paper variant="outlined" className={classes.mission}>
-      <Typography variant="h6" gutterBottom>
-        Happy Tails Mission
-      </Typography>
-      <Typography variant="body1">
-        Every year thousands of dogs are surrendered to shelters and rescues due
-        to behavioral issues. These issues typically stem from improper training
-        or knowledge of the breed and temperament of their dog. Here at Happy
-        Tails, it is our mission to match available dogs to prospective dog
-        owners based on their personal information and lifestyle. We hope that
-        by seeing how well one matches the available dogs they can make an
-        informed decision when choosing a dog. We want every tail to end
-        happily.
-      </Typography>
-    </Paper>
-  </Container>
-  )
+  return (
+    <Container maxWidth="md">
+      <Paper variant="outlined" className={classes.mission}>
+        <img src="dog-heart.jpg" />
+        <Typography variant="h6" gutterBottom>
+          Happy Tails Mission
+        </Typography>
+        <Typography variant="body1">
+          Every year thousands of dogs are surrendered to shelters and rescues
+          due to behavioral issues. These issues typically stem from improper
+          training or knowledge of the breed and temperament of their dog. Here
+          at Happy Tails, it is our mission to match available dogs to
+          prospective dog owners based on their personal information and
+          lifestyle. We hope that by seeing how well one matches the available
+          dogs they can make an informed decision when choosing a dog. We want
+          every tail to end happily.
+        </Typography>
+      </Paper>
+    </Container>
+  );
 };
 
-export default (InfoPage);
-
-
+export default InfoPage;
